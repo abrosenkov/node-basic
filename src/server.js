38b2366ec6e736d57ea71ec4d08b8ebe5e2507ef
abrosenkov <abrosenkov91@gmail.com>
2025-11-05 22:19:21 +0100

    add patch controller

--- src/server.js
+++ src/server.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 
-app.use(express.json());
+app.use(
+  express.json({
+    limit: '100kb',
+  }),
+);
 app.use(cors());
 
 app.use((req, res, next) => {
