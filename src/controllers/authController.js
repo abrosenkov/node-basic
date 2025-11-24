d90457bf1261f98077d0dcc080767f362469e186
abrosenkov <abrosenkov91@gmail.com>
2025-11-24 23:30:30 +0100

    add reset email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,8 @@ import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendEmail.js';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -91,5 +93,41 @@ export const refreshUserSession = async (req, res, next) => {
 
   res.status(200).json({
     message: 'Session refreshed',
+  });
+};
+
+export const requestResetEmail = async (req, res, next) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(200).json({
+      message: 'If this email exists, a reset link has been sent',
+    });
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+    });
+  } catch {
+    next(
+      createHttpError(500, 'Failed to send the email, please try again later.'),
+    );
+    return;
+  }
+
+  res.status(200).json({
+    message: 'Password reset email sent successfully',
   });
 };
