964d4e7d6268d3bb033f6524a923f7019d976613
abrosenkov <abrosenkov91@gmail.com>
2025-11-26 00:01:46 +0100

    add Handlebars

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,6 +6,10 @@ import { Session } from '../models/session.js';
 import jwt from 'jsonwebtoken';
 import { sendEmail } from '../utils/sendEmail.js';
 
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import handlebars from 'handlebars';
+
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
 
@@ -113,12 +117,23 @@ export const requestResetEmail = async (req, res, next) => {
     { expiresIn: '15m' },
   );
 
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+
+  const template = handlebars.compile(templateSource);
+
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
   try {
     await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
-      html: `<p>Click <a href="${resetToken}">here</a> to reset your password!</p>`,
+      html,
     });
   } catch {
     next(
